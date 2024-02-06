bfd4fefc81df2a2f39a5ec1bf554b31b0b2b1a09
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-02-06 10:59:05 +0800

    add form on add page

--- lib/widgets/add.dart
+++ lib/widgets/add.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class AddPage extends StatelessWidget {
-  const AddPage({super.key});
+  var nameEditingController = TextEditingController();
+  var descEditingController = TextEditingController();
+  var placeEditingContorller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Add new Item"),),
+      body: Column(
+        children: [
+          Text("Enter all the information"),
+          TextField(decoration: InputDecoration(hintText:"Enter item name"), controller: nameEditingController,),
+          TextField(decoration: InputDecoration(hintText:"Enter item description"), controller: descEditingController,),
+          TextField(decoration: InputDecoration(hintText:"Enter item place"), controller: placeEditingContorller,),
+          ElevatedButton(onPressed: (){
+            print(nameEditingController.text);
+            print(descEditingController.text);
+            print(placeEditingContorller.text);
+            // Return to previous page
+            Navigator.pop(context);
+
+          }, child: Text("Add new item"))
+        ],
+      ),
+    );
   }
 }
